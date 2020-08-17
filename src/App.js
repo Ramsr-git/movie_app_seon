3d0d176d54cb0c4249ab15b6e353332a48e9abca
Seonbox <60653941+Ramsr-git@users.noreply.github.com>
2020-08-17 22:53:28 +0900

    react 기본원리

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
 
+function Food({ e }) {
+  return <h3>I like {e}</h3>;
+}
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Food e="k" />
+      <Food e="z" />
+    </div>
+  );
 }
 
 export default App;
