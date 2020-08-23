2283c800b40e9a465c51d1335debac7cbe5e4ae7
Seonbox <60653941+Ramsr-git@users.noreply.github.com>
2020-08-23 20:00:02 +0900

    Movie Component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
-import PropTypes from "prop-types";
 
-function Food({ e }) {
-  return <h3>I like {e}</h3>;
-}
-
-function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-      <Food e="k" />
-      <Food e="z" />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true,
+  };
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
+  }
+  render() {
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+  }
 }
 
 export default App;
